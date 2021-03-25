22314fe78c9e3a50a2544336fde980e939816918
Gustavo Barreto <gbarreto.alves2@gmail.com>
2021-03-25 15:16:26 -0300

    First Google Clone's commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Meu clone do google com modo noturno.
---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+function nightMode(){
+
+    document.getElementById("body").style.backgroundColor ="#2a2a2e";
+    document.getElementById("gmailtext").style.color ="#ffffff";
+    document.getElementById("imagenstext").style.color ="#ffffff";
+    document.getElementById("input").style.backgroundColor ="#2a2a2e";
+    document.getElementById("input").style.color ="#ffffff";
+    document.getElementById("buttons_search").style.color ="#ffffff";
+    document.getElementById("buttons_search1").style.color ="#ffffff";
+    document.getElementById("language_text").style.color ="#ffffff";
+    document.getElementById("language_en").style.color ="#0060df";
+    document.getElementById("night_mode").style.color ="#ffffff";
+    document.getElementById("night_mode").style.backgroundColor ="#2a2a2e";
+    document.getElementById("normal_mode").style.backgroundColor ="#2a2a2e";
+    document.getElementById("normal_mode").style.color ="#ffffff";
+
+   
+}
+
+function returnNormal(){
+    document.getElementById("body").style.backgroundColor ="white";
+    document.getElementById("gmailtext").style.color ="black";
+    document.getElementById("imagenstext").style.color ="black";
+    document.getElementById("input").style.backgroundColor ="white";
+    document.getElementById("input").style.color ="black";
+    document.getElementById("buttons_search").style.color ="black";
+    document.getElementById("buttons_search1").style.color ="black";
+    document.getElementById("language_text").style.color ="black";
+    document.getElementById("language_en").style.color ="#4d0dc5";
+    document.getElementById("night_mode").style.color ="black";
+    document.getElementById("night_mode").style.backgroundColor ="white";
+    document.getElementById("normal_mode").style.backgroundColor ="white";
+    document.getElementById("normal_mode").style.color ="black";
+}
+
+
+
+
